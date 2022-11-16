08af5154a5a874ec023f2c1aa0ca0fe8a2c338a3
Martin Cordero <thisismartinc@gmail.com>
2022-11-15 19:01:00 -0500

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+		AppRoutingModule,
+		RouterModule.forRoot([
+			{ path: '', component: HomepageComponent },
+			{ path: 'login', component: LoginPageComponent },
+		]),
+	],
+	providers: [FormBuilder],
+	bootstrap: [AppComponent],
+	declarations: [AppComponent, HomepageComponent, LoginPageComponent],
 })
-export class AppModule { }
+export class AppModule {}
